aa731c9db75180d3e9808e997fe95b937ef1a18e
MSK1206 <phantom7216@yahoo.co.jp>
2023-02-23 17:44:35 +0900

    index fix .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { SeoHead } from "@/components/seohead";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
+  const pageOgImg: string = `${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}`;
   return (
     <>
       <SeoHead
@@ -9,7 +10,7 @@ export default function Home() {
         titleTemplate={"Sample Nextjs App"}
         description={"Next.js + TypeScript SEO編"}
         ogType={"website"}
-        imgUrl={"/next.jpg"}
+        imgUrl={`${pageOgImg}/next.jpg`}
       />
       <main className={styles.main}>
         <h1>Next.js + TypeScript Sample Project.</h1>

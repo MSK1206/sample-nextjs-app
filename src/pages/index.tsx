db414f5a9870f6c0dbfd1d55f88f1a6a7aaf8105
MSK1206 <phantom7216@yahoo.co.jp>
2023-02-23 15:31:46 +0900

    index fix .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { SeoHead } from "@/components/seohead";
 import styles from "@/styles/Home.module.css";
 
-export default function Home() {
+export const Home = () => {
   return (
     <>
       <SeoHead
@@ -9,11 +9,11 @@ export default function Home() {
         titleTemplate="Sample Nextjs App"
         description="Next.js + TypeScript SEO編"
         ogType="website"
-        imgUrl={`${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}/eyecatch.png`}
+        imgUrl={"https://sample-nextjs-app.vercel.app/eyecatch.png"}
       />
       <main className={styles.main}>
         <h1>Next.js + TypeScript Sample Project.</h1>
       </main>
     </>
   );
-}
+};
